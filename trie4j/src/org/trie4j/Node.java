9293743035cfef99f562f3c1ab34c26c132964a8
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2012-04-12 08:34:37 +0900

    add license notation.

--- trie4j/src/org/trie4j/Node.java
+++ trie4j/src/org/trie4j/Node.java
@@ -1,20 +1,22 @@
+/*
+ * Copyright 2012 Takao Nakaguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.trie4j;
 
 public interface Node {
 	char[] getLetters();
-	boolean isTerminated();
 	Node[] getChildren();
-/*	void setChildren(LeafNode[] children);
-	boolean hasFirstLetter();
-	char getFirstLetter();
-	int getLettersLength();
-	int getLettersLengthInNode();
-	Node insertChild(char[] letters, int offset);
-	Node pushLabel(LabelTrie trie);
-	boolean contains(char[] letters, int offset);
-
-	Node getChild(char c);
-	void visit(TrieVisitor visitor, int nest);
-	void addChild(int index, LeafNode n);
-*/
+	boolean isTerminated();
 }
