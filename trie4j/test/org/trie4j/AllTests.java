c44ec850b16f463668319bbfd58a38174ad0ce34
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2012-05-25 00:24:52 +0900

    add ignored dir(bin)

--- trie4j/test/org/trie4j/AllTests.java
+++ trie4j/test/org/trie4j/AllTests.java
@@ -6,12 +6,14 @@ import org.trie4j.doublearray.DoubleArrayTest;
 import org.trie4j.doublearray.TailCompactionDoubleArrayTest;
 import org.trie4j.doublearray.TailDoubleArrayTest;
 import org.trie4j.tail.SuffixTrieTailBuilderTest;
+import org.trie4j.util.BitVectorTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	DoubleArrayTest.class, TailCompactionDoubleArrayTest.class
 	, TailDoubleArrayTest.class
 	, SuffixTrieTailBuilderTest.class
+	, BitVectorTest.class
 })
 public class AllTests {
 
