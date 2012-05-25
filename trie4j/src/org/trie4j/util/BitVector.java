b485ea14c2dd83d3468d0f4f90fe60530587c620
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2012-05-26 00:39:36 +0900

    add license comments.

--- trie4j/src/org/trie4j/util/BitVector.java
+++ trie4j/src/org/trie4j/util/BitVector.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Takao Nakaguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.trie4j.util;
 
 public class BitVector {
@@ -43,14 +58,15 @@ public class BitVector {
 		}
 	}
 
-	// select(count, bit)はbitがcount回出てくる位置を返す。つまりrank()の逆関数。
 	public int select(int count, boolean b){
 		if(b){
 			for(int i = 0; i < vector.length; i++){
+				if(i * 8 >= size) return -1;
 				int c = BITCOUNTS[vector[i] & 0xff];
 				if(count <= c){
 					int v = vector[i] & 0xff;
 					for(int j = 0; j < 8; j++){
+						if(i * 8 + j >= size) return -1;
 						if((v & 0x80) != 0){
 							count--;
 							if(count == 0){
@@ -64,10 +80,12 @@ public class BitVector {
 			}
 		} else{
 			for(int i = 0; i < vector.length; i++){
+				if(i * 8 >= size) return -1;
 				int c = 8 - BITCOUNTS[vector[i] & 0xff];
 				if(count <= c){
 					int v = vector[i] & 0xff;
 					for(int j = 0; j < 8; j++){
+						if(i * 8 + j >= size) return -1;
 						if((v & 0x80) == 0){
 							count--;
 							if(count == 0){
--- trie4j/test/org/trie4j/util/BitVectorTest.java
+++ trie4j/test/org/trie4j/util/BitVectorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Takao Nakaguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.trie4j.util;
 
 import junit.framework.Assert;
@@ -8,7 +23,7 @@ public class BitVectorTest {
 	@Test
 	public void test_rank() throws Exception{
 		BitVector bv = new BitVector(16);
-		for(int i = 0; i < 64; i++){
+		for(int i = 0; i < 135; i++){
 			bv.append(true);
 			Assert.assertEquals(i + 1, bv.rank(i, true));
 			Assert.assertEquals(0, bv.rank(i, false));
@@ -26,5 +41,28 @@ public class BitVectorTest {
 		bv.append(false);
 		Assert.assertEquals(4, bv.select(4, true));
 		Assert.assertEquals(2, bv.select(1, false));
+	}
+
+	@Test
+	public void test_select_2() throws Exception{
+		BitVector bv = new BitVector(1);
+		for(int i = 0; i < 8; i++){
+			bv.append(true);
+			bv.append(true);
+			bv.append(false);
+		}
+		Assert.assertEquals(16, bv.select(12, true));
+		Assert.assertEquals(14, bv.select(5, false));
+	}
+
+	@Test
+	public void test_select_fail_1() throws Exception{
+		BitVector bv = new BitVector(1);
+		Assert.assertEquals(-1, bv.select(9, true));
+		Assert.assertEquals(-1, bv.select(1, false));
+		bv.append(false);
+		Assert.assertEquals(-1, bv.select(9, true));
+		Assert.assertEquals(0, bv.select(1, false));
+		Assert.assertEquals(-1, bv.select(2, false));
 	}
 }
