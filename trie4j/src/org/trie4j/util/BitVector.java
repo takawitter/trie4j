d05f8de7fe2c491caad958d0de9422710a03d58a
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2012-06-05 00:05:52 +0900

    improve performance with cache and implement next0 method.

--- trie4j/src/org/trie4j/util/BitVector.java
+++ trie4j/src/org/trie4j/util/BitVector.java
@@ -15,10 +15,29 @@
  */
 package org.trie4j.util;
 
+
 public class BitVector {
+	public BitVector(){
+		this(16);
+	}
+
+	public BitVector(byte[] bytes, int size){
+		this.vector = bytes.clone();
+		this.size = size;
+		this.cache = new int[size / CACHE_WIDTH + 1];
+		int n = size / 8;
+		for(int i = 0; i < n; i++){
+			int ci = i / CACHE_WIDTH / 8;
+			if(ci > 0 && (i % (CACHE_WIDTH / 8)) == 0){
+				cache[ci] = cache[ci - 1];
+			}
+			cache[ci] += BITCOUNTS[vector[i] & 0xff];
+		}
+	}
+
 	public BitVector(int initialCapacity){
 		 vector = new byte[initialCapacity / 8 + 1];
-		 cache = new int[initialCapacity / WIDTH];
+		 cache = new int[initialCapacity / CACHE_WIDTH + 1];
 	}
 
 	public int size(){
@@ -26,35 +45,58 @@ public class BitVector {
 	}
 	
 	public void trimToSize(){
-		int sz = size / 8 + 1;
-		if(vector.length > sz){
-			byte[] nv = new byte[sz];
-			System.arraycopy(vector, 0, nv, 0, sz);
-			vector = nv;
+		int vectorSize = size / 8 + 1;
+		byte[] nv = new byte[vectorSize];
+		System.arraycopy(vector, 0, nv, 0, Math.min(vector.length, vectorSize));
+		vector = nv;
+		int cacheSize = vectorSize / (CACHE_WIDTH / 8) + 1;
+		int[] nc = new int[cacheSize];
+		System.arraycopy(cache, 0, nc, 0, Math.min(cache.length, cacheSize));
+		cache = nc;
+	}
+
+	public void append1(){
+		int i = size / 8;
+		int ci = size / CACHE_WIDTH;
+		if(i >= vector.length){
+			extend();
 		}
+		if(size % CACHE_WIDTH == 0 && ci > 0){
+			cache[ci] = cache[ci - 1];
+		}
+		int r = size % 8;
+		vector[i] |= BITS[r];
+		cache[ci]++;
+		size++;
+	}
+
+	public void append0(){
+		int i = size / 8;
+		int ci = size / CACHE_WIDTH;
+		if(i >= vector.length){
+			extend();
+		}
+		if(size % CACHE_WIDTH == 0 && ci > 0){
+			cache[ci] = cache[ci - 1];
+		}
+		int r = size % 8;
+		vector[i] &= ~BITS[r];
+		size++;
 	}
 
 	public void append(boolean bit){
-		int i = size / 8;
-		if(i >= vector.length){
-			byte[] n = new byte[(int)(vector.length * 1.2) + 1];
-			System.arraycopy(vector, 0, n, 0, vector.length);
-			n[vector.length] = 0;
-			vector = n;
-		}
-		int r = size % 8;
-		if(bit){
-			vector[i] |= BITS[r];
-		} else{
-			vector[i] &= ~BITS[r];
-		}
-		size++;
+		if(bit) append1();
+		else append0();
 	}
 
 	public int rank1(int pos){
 		int ret = 0;
+		int cn = pos / CACHE_WIDTH;
+		if(cn > 0){
+			ret = cache[cn - 1];
+		}
 		int n = pos / 8;
-		for(int i = 0; i < n; i++){
+		for(int i = (cn * 8); i < n; i++){
 			ret += BITCOUNTS[vector[i] & 0xff];
 		}
 		ret += BITCOUNTS[vector[n] & MASKS[pos % 8]];
@@ -63,8 +105,12 @@ public class BitVector {
 
 	public int rank0(int pos){
 		int ret = 0;
+		int cn = pos / CACHE_WIDTH;
+		if(cn > 0){
+			ret = cn * CACHE_WIDTH - cache[cn - 1];
+		}
 		int n = pos / 8;
-		for(int i = 0; i < n; i++){
+		for(int i = (cn * 8); i < n; i++){
 			ret += 8 - BITCOUNTS[vector[i] & 0xff];
 		}
 		ret += ((pos % 8) + 1) - BITCOUNTS[vector[n] & MASKS[pos % 8]];
@@ -75,25 +121,68 @@ public class BitVector {
 		if(b) return rank1(pos);
 		else return rank0(pos);
 	}
-	
+
 	public int select0(int count){
-		for(int i = 0; i < vector.length; i++){
-			if(i * 8 >= size) return -1;
-			int c = 8 - BITCOUNTS[vector[i] & 0xff];
-			if(count <= c){
-				int v = vector[i] & 0xff;
-				for(int j = 0; j < 8; j++){
-					if(i * 8 + j >= size) return -1;
-					if((v & 0x80) == 0){
-						count--;
-						if(count == 0){
-							return i * 8 + j;
-						}
-					}
-					v <<= 1;
-				}
+		int start = 0;
+		int end = cache.length;
+		int m = 0;
+		int d = 0;
+		while(start != end){
+			m = (start + end) / 2;
+			d = count - (CACHE_WIDTH * (m + 1) - cache[m]);
+			if(m == (cache.length - 1)){
+				d += CACHE_WIDTH - (size % CACHE_WIDTH);
 			}
-			count -= c;
+			if(d < 0){
+				end = m;
+				continue;
+			} else if(d > 0){
+				if(start != m) start = m;
+				else break;
+			} else{
+				break;
+			}
+		}
+		if(d > 0){
+			count = d;
+			for(int i = (m + 1) * CACHE_WIDTH / 8; i < vector.length; i++){
+				if(i * 8 >= size) return -1;
+				int c = 8 - BITCOUNTS[vector[i] & 0xff];
+				if(count <= c){
+					int v = vector[i] & 0xff;
+					for(int j = 0; j < 8; j++){
+						if(i * 8 + j >= size) return -1;
+						if((v & 0x80) == 0){
+							count--;
+							if(count == 0){
+								return i * 8 + j;
+							}
+						}
+						v <<= 1;
+					}
+				}
+				count -= c;
+			}
+		} else{
+			count = d - 1;
+			int i = Math.min(((m + 1) * CACHE_WIDTH) - 1, size - 1);
+			int v = vector[i / 8] & 0xff;
+			v >>= 8 - (i % 8) - 1;
+			while(i >= 0){
+				if((v & 0x01) == 0){
+					count++;
+					if(count == 0){
+						return i;
+					}
+				}
+				v >>= 1;
+				if(i % 8 == 0){
+					v = vector[(i - 1) / 8] & 0xff;
+				}
+				i--;
+			}
+//				count = CACHE_WIDTH * (m + 1) - cache[m] - (CACHE_WIDTH * m - cache[m - 1]);
+//				count = CACHE_WIDTH - cache[m] + cache[m - 1];
 		}
 		return -1;
 	}
@@ -125,7 +214,48 @@ public class BitVector {
 		else return select0(count);
 	}
 
-	private static final int WIDTH = 32;
+	public int next0(int pos){
+		int i = pos / 8;
+		{
+			int v = vector[i] & 0xff;
+			v <<= pos % 8;
+			for(int j = pos % 8; j < 8; j++){
+				if(i * 8 + j >= size) return -1;
+				if((v & 0x80) == 0){
+					return i * 8 + j;
+				}
+				v <<= 1;
+			}
+		}
+		for(i++; i < vector.length; i++){
+			if(i * 8 >= size) return -1;
+			int c = 8 - BITCOUNTS[vector[i] & 0xff];
+			if(1 <= c){
+				int v = vector[i] & 0xff;
+				for(int j = 0; j < 8; j++){
+					if(i * 8 + j >= size) return -1;
+					if((v & 0x80) == 0){
+						return i * 8 + j;
+					}
+					v <<= 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	private void extend(){
+		int vectorSize = (int)(vector.length * 1.2) + 1;
+		byte[] nv = new byte[vectorSize];
+		System.arraycopy(vector, 0, nv, 0, vector.length);
+		vector = nv;
+		int cacheSize = vectorSize / (CACHE_WIDTH / 8) + 1;
+		int[] nc = new int[cacheSize];
+		System.arraycopy(cache, 0, nc, 0, cache.length);
+		cache = nc;
+	}
+
+	private static final int CACHE_WIDTH = 64;
 	private byte[] vector;
 	private int size;
 	private int[] cache;
--- trie4j/test/org/trie4j/util/BitVectorTest.java
+++ trie4j/test/org/trie4j/util/BitVectorTest.java
@@ -23,15 +23,15 @@ public class BitVectorTest {
 	@Test
 	public void test_rank() throws Exception{
 		BitVector bv = new BitVector(16);
-		for(int i = 0; i < 135; i++){
-			bv.append(true);
-			Assert.assertEquals(i + 1, bv.rank(i, true));
-			Assert.assertEquals(0, bv.rank(i, false));
+		for(int i = 0; i < 2048; i++){
+			bv.append(i % 2 == 0);
+			Assert.assertEquals(i / 2 + 1, bv.rank1(i));
+			Assert.assertEquals(i / 2 + i % 2, bv.rank0(i));
 		}
 	}
 
 	@Test
-	public void test_select() throws Exception{
+	public void test_select0_1() throws Exception{
 		BitVector bv = new BitVector(16);
 		bv.append(true);
 		bv.append(true);
@@ -39,30 +39,131 @@ public class BitVectorTest {
 		bv.append(true);
 		bv.append(true);
 		bv.append(false);
-		Assert.assertEquals(4, bv.select(4, true));
-		Assert.assertEquals(2, bv.select(1, false));
+		Assert.assertEquals(2, bv.select0(1));
 	}
 
 	@Test
-	public void test_select_2() throws Exception{
+	public void test_select0_2() throws Exception{
 		BitVector bv = new BitVector(1);
-		for(int i = 0; i < 8; i++){
+		for(int i = 0; i < 2000; i++){
 			bv.append(true);
 			bv.append(true);
 			bv.append(false);
 		}
-		Assert.assertEquals(16, bv.select(12, true));
-		Assert.assertEquals(14, bv.select(5, false));
+		Assert.assertEquals(14, bv.select0(5));
+		Assert.assertEquals(59, bv.select0(20));
+		Assert.assertEquals(89, bv.select0(30));
+		Assert.assertEquals(104, bv.select0(35));
+		Assert.assertEquals(299, bv.select0(100));
+		Assert.assertEquals(1076, bv.select0(359));
+		Assert.assertEquals(3899, bv.select0(1300));
+	}
+
+	@Test
+	public void test_select1_1() throws Exception{
+		BitVector bv = new BitVector(1);
+		for(int i = 0; i < 2000; i++){
+			bv.append(true);
+			bv.append(true);
+			bv.append(false);
+		}
+		Assert.assertEquals(0, bv.select1(1));
+		Assert.assertEquals(4, bv.select1(4));
+		Assert.assertEquals(10, bv.select1(8));
+		Assert.assertEquals(16, bv.select1(12));
+		Assert.assertEquals(1948, bv.select1(1300));
 	}
 
 	@Test
 	public void test_select_fail_1() throws Exception{
 		BitVector bv = new BitVector(1);
-		Assert.assertEquals(-1, bv.select(9, true));
-		Assert.assertEquals(-1, bv.select(1, false));
+		Assert.assertEquals(-1, bv.select1(9));
+		Assert.assertEquals(-1, bv.select0(1));
+		bv.append0();
+		Assert.assertEquals(-1, bv.select1(9));
+		Assert.assertEquals(0, bv.select0(1));
+		Assert.assertEquals(-1, bv.select0(2));
+	}
+
+	@Test
+	public void test_next0_1() throws Exception{
+		BitVector bv = new BitVector();
 		bv.append(false);
-		Assert.assertEquals(-1, bv.select(9, true));
-		Assert.assertEquals(0, bv.select(1, false));
-		Assert.assertEquals(-1, bv.select(2, false));
+		bv.append(false);
+		Assert.assertEquals(0, bv.next0(0));
+		Assert.assertEquals(1, bv.next0(1));
+	}
+
+	@Test
+	public void test_next0_2() throws Exception{
+		BitVector bv = new BitVector();
+		bv.append(true);
+		bv.append(false);
+		bv.append(true);
+		bv.append(false);
+		Assert.assertEquals(1, bv.next0(0));
+		Assert.assertEquals(3, bv.next0(2));
+	}
+
+	@Test
+	public void test_next0_3() throws Exception{
+		BitVector bv = new BitVector();
+		for(int i = 0; i < 8; i++){
+			bv.append(true);
+		}
+		bv.append(false);
+		Assert.assertEquals(8, bv.next0(0));
+	}
+
+	@Test
+	public void test_next0_4() throws Exception{
+		BitVector bv = new BitVector();
+		for(int i = 0; i < 130; i++){
+			bv.append(true);
+		}
+		bv.append(false);
+		Assert.assertEquals(130, bv.next0(0));
+	}
+
+	@Test
+	public void test_next0_5() throws Exception{
+		BitVector bv = new BitVector();
+		for(int i = 0; i < 63; i++){
+			bv.append(true);
+		}
+		bv.append(false);
+		bv.append(false);
+		Assert.assertEquals(63, bv.next0(0));
+		Assert.assertEquals(64, bv.next0(64));
+	}
+
+	@Test
+	public void test_next0_6() throws Exception{
+		BitVector bv = new BitVector(new byte[]{-69, 0}, 13);
+		Assert.assertEquals(8, bv.next0(6));
+	}
+
+	@Test
+	public void test_hugedata_rank1() throws Exception{
+		int size = 1000000;
+		BitVector bv = new BitVector(size);
+		for(int i = 0; i < size; i++){
+			bv.append(true);
+		}
+		for(int i = 0; i < 100000; i++){
+			Assert.assertEquals(size, bv.rank1(size - 1));
+		}
+	}
+
+	@Test
+	public void test_hugedata_select0() throws Exception{
+		int size = 1000000;
+		BitVector bv = new BitVector(size);
+		for(int i = 0; i < size; i++){
+			bv.append(false);
+		}
+		for(int i = 0; i < 100000; i++){
+			Assert.assertEquals(size - 1, bv.select0(size));
+		}
 	}
 }
