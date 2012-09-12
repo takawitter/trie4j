3f8a4211ea6d2b32f0b29bed1f1b99b61b61075d
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2012-09-12 21:06:32 +0900

    change to PatriciaTrie

--- trie4j/test/org/trie4j/util/BitVectorExp.java
+++ trie4j/test/org/trie4j/util/BitVectorExp.java
@@ -3,9 +3,9 @@ package org.trie4j.util;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.trie4j.Node;
-import org.trie4j.Trie;
 import org.trie4j.NodeVisitor;
-import org.trie4j.patricia.multilayer.MultilayerPatriciaTrie;
+import org.trie4j.Trie;
+import org.trie4j.patricia.simple.PatriciaTrie;
 import org.trie4j.test.LapTimer;
 import org.trie4j.test.WikipediaTitles;
 
@@ -13,19 +13,19 @@ public class BitVectorExp {
 	private static final int maxCount = 2000000;
 
 	public static void main(String[] args) throws Exception{
-		Trie trie = new MultilayerPatriciaTrie();
+		Trie trie = new PatriciaTrie();
 		int c = 0;
 		// You can download archive from http://dumps.wikimedia.org/jawiki/latest/
 		LapTimer t = new LapTimer();
 		for(String word : new WikipediaTitles(
-				"jawiki-20120220-all-titles-in-ns0.gz"
+				"data/jawiki-20120220-all-titles-in-ns0.gz"
 				//"enwiki-20120403-all-titles-in-ns0.gz"
 				)){
 			trie.insert(word);
 			c++;
 			if(c == maxCount) break;
 		}
-		t.lap("trie building done. $d words.", c);
+		t.lap("trie building done. %d words.", c);
 		final SuccinctBitVector bv = new SuccinctBitVector(5000000);
 		final AtomicInteger nodeCount = new AtomicInteger();
 		trie.traverse(new NodeVisitor() {
