ea42b3635b800a75048cd60dc9610c885b8df456
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2013-05-09 20:41:58 +0900

    Test for FastBitSet

---
+++ trie4j/test/org/trie4j/util/FastBitSetTest.java
@@ -0,0 +1,47 @@
+package org.trie4j.util;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class FastBitSetTest {
+	@Test
+	public void test_set_1() throws Exception{
+		FastBitSet bs = new FastBitSet();
+		for(int i = 0; i < 1000; i++){
+			bs.set(i);
+			Assert.assertTrue(bs.get(i));
+		}
+	}
+
+	@Test
+	public void test_set_2() throws Exception{
+		FastBitSet bs = new FastBitSet();
+		for(int i = 0; i < 8; i++){
+			int index = (int)Math.pow(10, i);
+			bs.set(index);
+			Assert.assertTrue(bs.get(index));
+		}
+	}
+
+	@Test
+	public void test_unset_1() throws Exception{
+		FastBitSet bs = new FastBitSet();
+		for(int i = 0; i < 1000; i++){
+			bs.set(i);
+			bs.unset(i);
+			Assert.assertFalse(bs.get(i));
+		}
+	}
+
+	@Test
+	public void test_unset_2() throws Exception{
+		FastBitSet bs = new FastBitSet();
+		for(int i = 0; i < 8; i++){
+			int index = (int)Math.pow(10, i);
+			bs.set(index);
+			bs.unset(index);
+			Assert.assertFalse(bs.get(index));
+		}
+	}
+}
